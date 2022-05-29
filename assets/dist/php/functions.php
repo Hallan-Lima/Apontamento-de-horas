9cedccb1b87041376e6eb9aede58a01ed89c296f
Hallan-Lima <54196012+Hallan-Lima@users.noreply.github.com>
2022-05-29 11:10:53 -0300

    ajuste de url

--- assets/dist/php/functions.php
+++ assets/dist/php/functions.php
@@ -98,7 +98,7 @@ function navHeader()
 {
     echo '
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Apontamento de horas</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="index">Apontamento de horas</a>
     </header>
     ';
 }
